9b2dc76011ca608664dac8851ad0b5c6fa0eb781
Ubuntu <ubuntu@ip-172-31-43-177.us-east-2.compute.internal>
2021-05-31 19:33:38 +0000

    today

--- MyFirstContainer/SecondContainer_FIndCode/src/writetxt.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-dirname = os.path.dirname(__file__)
-codefilePath = os.path.join(dirname, 'output/' , 'mytext.txt')
-
-with open(codefilePath,"w") as file:
-    file.write("I know how to use docker.")
