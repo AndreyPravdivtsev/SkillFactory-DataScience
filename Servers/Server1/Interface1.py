17c1fbf0cd88b572aea727409a1cebad0fbec744
AndreyPravdivtsev <67222421+AndreyPravdivtsev@users.noreply.github.com>
2021-06-02 22:42:42 +0200

    Update Interface1.py
    final interface 1

--- Servers/Server1/Interface1.py
+++ Servers/Server1/Interface1.py
@@ -2,9 +2,12 @@ import numpy as np
 import json
 from sklearn.datasets import load_diabetes
 import pika
-
+# start in the other terminal
+# sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 X, y = load_diabetes(return_X_y=True)
 random_row = np.random.randint(0, X.shape[0]-1)
+
+print(f'Random diabetes row is {random_row}')
 
 # Подключение к серверу на локальном хосте:
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -22,7 +25,8 @@ message_serial = json.dumps(message)
 channel.basic_publish(exchange='',
                       routing_key='y_true',
                       body=message_serial)
-
+print(f'Random diabetes value {message}')
+print(f'Random diabetes serialized value {message_serial}')
 
 channel.queue_declare(queue='Features')
 message = list(X[random_row])
@@ -30,6 +34,8 @@ message_serial = json.dumps(message)
 channel.basic_publish(exchange='',
                       routing_key='Features',
                       body=message_serial)
+print(f'Random diabetes features {message}')
+print(f'Random diabetes serialized features {message_serial}')
 
 print('Сообщение с правильным ответом, отправлено в очередь')
 # Закроем подключение 
